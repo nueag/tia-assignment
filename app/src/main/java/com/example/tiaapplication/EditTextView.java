409eb0f366db8f6d0981e7a9bf0f9ecb0bea0070
nueag <kaeun0000@naver.com>
2024-06-07 10:41:31 +0900

    12nd assignment

--- app/src/main/java/com/example/tiaapplication/EditTextView.java
+++ app/src/main/java/com/example/tiaapplication/EditTextView.java
@@ -3,14 +3,18 @@ package com.example.tiaapplication;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Rect;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.text.Editable;
+import android.text.InputType;
 import android.text.TextWatcher;
+import android.text.method.PasswordTransformationMethod;
 import android.util.Log;
 import android.view.KeyEvent;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 
@@ -19,6 +23,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 
 public class EditTextView extends AppCompatActivity {
+    private boolean isPasswordVisible;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,6 +33,11 @@ public class EditTextView extends AppCompatActivity {
         close_btn.setOnClickListener(v -> finish());
 
         SignUpBox phoneNumberInput = findViewById(R.id.phone_number_box);
+        EditText phonEditText = phoneNumberInput.getEditText();
+        Button visibleButton = findViewById(R.id.visible_button);
+        visibleButton.setVisibility(View.INVISIBLE);
+
+        isPasswordVisible = false;
         phoneNumberInput.getEditText().addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {
@@ -36,11 +46,32 @@ public class EditTextView extends AppCompatActivity {
             @Override
             public void onTextChanged(CharSequence s, int start, int before, int count) {
                 Log.d("test", "input : " + s.toString());
+
             }
 
             @Override
             public void afterTextChanged(Editable s) {
+                if(s.length()>0) {
+                    findViewById(R.id.visible_button).setVisibility(View.VISIBLE);
+                } else {
+                    findViewById(R.id.visible_button).setVisibility(View.INVISIBLE);
+                }
             }
+        });
+
+        visibleButton.setOnClickListener(v -> {
+            if (isPasswordVisible) {
+                phonEditText.setTransformationMethod(PasswordTransformationMethod.getInstance());
+                phonEditText.setInputType(InputType.TYPE_CLASS_NUMBER | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+                visibleButton.setBackground(getResources().getDrawable(R.drawable.visible));
+                isPasswordVisible = false;
+            } else {
+                phonEditText.setTransformationMethod(null);
+                phonEditText.setInputType(InputType.TYPE_CLASS_NUMBER | InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD);
+                visibleButton.setBackground(getResources().getDrawable(R.drawable.invisible));
+                isPasswordVisible = true;
+            }
+            phonEditText.setSelection(phoneNumberInput.getEditText().getText().length());
         });
 
     }
